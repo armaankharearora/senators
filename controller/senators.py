a497c6a9913998b6cc1e86b640c6a3a432e4d405
Armaan <76634103+armaankharearora@users.noreply.github.com>
2021-01-25 18:19:47 -0800

    added to summary and changed bipar to topicality

--- controller/senators.py
+++ controller/senators.py
@@ -16,6 +16,10 @@ import pylab as pyl
 import streamlit.components.v1 as components
 
 from pyvis.network  import Network
+
+stopwords = set(STOPWORDS)
+stopwords.update(["may", "US", "https", "t", "co", "RT", "S", 'U', 'amp', 'must' 'will', 've', 'si02', 'today', 'Today', 'See', 'FY21', 'GovKemp', 'de', 'lo', 'la', 'le', 'el', 'Si', 're', 'QampA', 'the', 'fwd', 'ovr', 'm', 'yet'])
+
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 add_selectbox = st.sidebar.selectbox(
@@ -23,8 +27,42 @@ add_selectbox = st.sidebar.selectbox(
     ('Senator View', 'Summary')
 )
 if add_selectbox == 'Summary':
-    bipar_df = pd.read_csv('model/bipar_scores.csv')
-    st.dataframe(bipar_df)
+    topic_df = pd.read_csv('model/bipar_scores.csv')
+    st.dataframe(topic_df)
+    topic_top_5 = topic_df.nlargest(5, 'score')
+    st.write(topic_top_5)
+
+    text1 = ""
+    for index, row in topic_top_5.iterrows():
+        f = open(f"model/raw_data/{row['name']}.txt", "r", encoding= "utf-8")
+        text = f.read()
+        f.close()
+        text1 = text1 + '\n' + text
+
+    wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text1)
+        # Display the generated image:
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    st.pyplot()
+
+    topic_bottom_5 = topic_df.nsmallest(5, 'score')
+    st.write(topic_bottom_5)
+
+    text2 = ""
+    for index, row in topic_bottom_5.iterrows():
+        f = open(f"model/raw_data/{row['name']}.txt", "r", encoding= "utf-8")
+        text = f.read()
+        f.close()
+        text2 = text2 + '\n' + text
+
+    wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text2)
+        # Display the generated image:
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis("off")
+    plt.show()
+    st.pyplot()
+
 
 else:
     def closest_point(a, b, c, x_0, y_0):
@@ -62,25 +100,25 @@ else:
 
     ave_par_sim = party_df['cosine_sim'][:5].mean()
     ave_not_par_sim = not_party_df['cosine_sim'][:5].mean()
-    st.subheader('Bipartisan Bridge Index')
+    st.subheader('Topicality Score')
     #score = (ave_not_par_sim / ave_par_sim)*100
     cords = closest_point(1, -1, 0, ave_par_sim, ave_not_par_sim)
     score = ((cords[0]+cords[1])/2) * 100
-    score_txt = f'This Senators Score is {score:.2f}'
+    score_txt = f"This Senator's Score is {score:.2f}"
     st.markdown(score_txt)
+    st.markdown("<i> This score represents topics that are of intrest to both parties; it does not indicate consensus </i>", unsafe_allow_html=True)
 
+    ######Section for WordCloud #####
     text = ""
     #public_tweets = api.user_timeline(twitterhandle)
     #for tweet in public_tweets:
         #print(tweet.text)
         #text = text + " " + tweet.text
-    st.subheader('Wordcloud representing the most used words from the Senators recent tweets')
+    st.subheader("Wordcloud representing the most used words from the Senator's recent tweets")
 
     f = open(f"model/raw_data/{twitterhandle}.txt", "r", encoding= "utf-8")
     text = f.read()
     f.close()
-    stopwords = set(STOPWORDS)
-    stopwords.update(["may", "US", "https", "t", "co", "RT", "S", 'U', 'amp', 'must' 'will', 've', 'si02', 'PPP', 'FY21', 'GovKemp', 'el', 'Si', 're', 'QampA', 'the', 'fwd', 'ovr'])
     wordcloud = WordCloud(stopwords=stopwords, background_color="black").generate(text)
 
     # Display the generated image:
